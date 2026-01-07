158cf9fef7eb7e05266cdf0d2c309ed92ae60782
Dominik <dominik@stride-services.pl>
2026-01-07 12:45:35 +0100

    feat: Add Stride logo to sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -51,12 +51,11 @@ export default function Sidebar() {
       {/* Logo */}
       <div className="p-6 border-b border-border">
         <Link href="/dashboard" className="flex items-center gap-3">
-          <div className="w-8 h-8 rounded-lg bg-white flex items-center justify-center">
-            <span className="text-black font-bold text-lg">S</span>
-          </div>
-          <h1 className="text-xl font-bold text-white">
-            Stride Admin
-          </h1>
+          <img
+            src="/logo.png"
+            alt="Stride"
+            className="h-7 w-auto"
+          />
         </Link>
       </div>
 
